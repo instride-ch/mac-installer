eaacae93ed81fcb56e0629f34b42469ae6393afd
Simon-Drohsen <simon.drohsen@instride.ch>
2025-06-23 15:36:54 +0200

    refactor: update Brewfile and instride_pl.sh, add Microsoft Outlook and remove unused casks

--- bin/instride_pl.sh
+++ bin/instride_pl.sh
@@ -54,7 +54,6 @@ if [[ ! "$(command -v nvm)" ]]; then
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    export source="~/.nvm/nvm.sh"
 
 fi
 
