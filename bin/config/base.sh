aeef087e04237a5c8455741d037b84d9afe8b5da
Simon <simondrohsend@gmail.com>
2025-06-24 12:06:32 +0200

    fix: readded ToDos

--- bin/config/base.sh
+++ bin/config/base.sh
@@ -22,6 +22,7 @@ defaults write com.apple.finder QLEnableTextSelection -bool TRUE
 
 #"Disabling OS X Gate Keeper"
 #"(You'll be able to install any app you want from here on, not just Mac App Store apps)"
+# ToDo: Do we want to disable it?
 sudo spctl --master-disable
 sudo defaults write /var/db/SystemPolicy-prefs.plist enabled -string no
 defaults write com.apple.LaunchServices LSQuarantine -bool false
@@ -38,29 +39,41 @@ defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 #"Check for software updates daily, not just once per week"
+# ToDo: How we want to deal with that?
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
 #"Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+# ToDo: Check if this makes sense - is disabled on my macbook
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 #"Disabling press-and-hold for keys in favor of a key repeat"
+# ToDo: Do we want that? - is enabled on my macbook
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
+#"Setting trackpad & mouse speed to a reasonable number"
+# ToDo: Check if this makes sense
+defaults write -g com.apple.trackpad.scaling 2
+defaults write -g com.apple.mouse.scaling 2.5
+
 #"Enabling subpixel font rendering on non-Apple LCDs"
+# ToDo: check out
 defaults write NSGlobalDomain AppleFontSmoothing -int 2
 
 #"Showing icons for hard drives, servers, and removable media on the desktop"
+# ToDo: design question
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 
 #"Avoiding the creation of .DS_Store files on network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 #"Enabling snap-to-grid for icons on the desktop and in other icon views"
+# ToDo: Remove?
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 
 #"Enabling UTF-8 ONLY in Terminal.app and setting the Pro theme by default"
+# ToDo: Check
 defaults write com.apple.terminal StringEncodings -array 4
 defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
 defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
@@ -97,6 +110,7 @@ defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebK
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
 # Don’t automatically rearrange Spaces based on most recent use
+# ToDo: Needed?
 defaults write com.apple.dock mru-spaces -bool false
 
 
